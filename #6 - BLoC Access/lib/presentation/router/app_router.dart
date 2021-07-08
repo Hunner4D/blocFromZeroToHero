39b303e64f962aaf37209eaf2502b6f4fb1b7301
Hunter Herrera <h.herrera.academics@gmail.com>
2021-07-08 12:59:18 -0700

    Blocl provided globally through material app

--- #6 - BLoC Access/lib/presentation/router/app_router.dart
+++ #6 - BLoC Access/lib/presentation/router/app_router.dart
@@ -7,48 +7,34 @@ import 'package:flutter_bloc_concepts/presentation/screens/second_screen.dart';
 import 'package:flutter_bloc_concepts/presentation/screens/third_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
 
   Route onGenerateRoute(RouteSettings settings) {
     final GlobalKey<ScaffoldState> key = settings.arguments;
     switch (settings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: HomeScreen(
-                      title: "Home Screen",
-                      color: Colors.blueAccent,
-                    ),
-          ),
+          builder: (_) => HomeScreen(
+                    title: "Home Screen",
+                    color: Colors.blueAccent,
+                  ),
         );
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: SecondScreen(
-                      title: "Second Screen",
-                      color: Colors.redAccent,
-                      homeScreenKey: key,
-                    ),
-          ),
+          builder: (_) => SecondScreen(
+                    title: "Second Screen",
+                    color: Colors.redAccent,
+                    homeScreenKey: key,
+                  ),
         );
       case '/third':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: ThirdScreen(
-                      title: "Thirst Screen",
-                      color: Colors.greenAccent,
-                    ),
-          ),
+          builder: (_) => ThirdScreen(
+                    title: "Thirst Screen",
+                    color: Colors.greenAccent,
+                  ),
         );
       default:
         return null;
     }
-  }
-
-  void dispose() {
-    _counterCubit.close();
   }
 }
