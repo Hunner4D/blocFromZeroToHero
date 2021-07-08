f39bb1a14484d6792046d2b12a33461ba0004900
Hunter Herrera <h.herrera.academics@gmail.com>
2021-07-08 12:51:46 -0700

    bloc routes generator

--- #6 - BLoC Access/lib/main.dart
+++ #6 - BLoC Access/lib/main.dart
@@ -20,49 +20,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   final AppRouter _appRouter = AppRouter();
 
-  final CounterCubit _counterCubit = CounterCubit();
-
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<CounterCubit>(
-      create: (context) => CounterCubit(),
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        onGenerateRoute: _appRouter.onGenerateRoute,
-        routes: {
-          '/': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: HomeScreen(
-                  title: "First Screen",
-                  color: Colors.red,
-                ),
-              ),
-          '/second': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: SecondScreen(
-                  title: "Second Screen",
-                  color: Colors.green,
-                ),
-              ),
-          '/third': (context) => BlocProvider.value(
-                value: _counterCubit,
-                child: ThirdScreen(
-                  title: "Third Screen",
-                  color: Colors.blue,
-                ),
-              ),
-        },
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
+      onGenerateRoute: _appRouter.onGenerateRoute,
     );
   }
 
   @override
   void dispose() {
-    _counterCubit.close();
+    _appRouter.dispose();
     super.dispose();
   }
 }
